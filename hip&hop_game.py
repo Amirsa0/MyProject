86a9f63d16d6643aae68f0de5b4511ff6e2e0c6e
Amirsa0 <144153397+Amirsa0@users.noreply.github.com>
2025-11-04 23:05:14 +0330

    Add files via upload

---
+++ hip&hop_game.py
@@ -0,0 +1,17 @@
+n = 0
+while n < 20 :
+    n+=1
+
+    if n % 3 == 0 and n % 5 == 0 : 
+        print("hiphop")
+        continue
+    if n % 3 == 0 : 
+        print("hop")
+        continue
+    if n % 5 == 0 : 
+        print("hip")
+        continue
+
+    print(n)
+
+
